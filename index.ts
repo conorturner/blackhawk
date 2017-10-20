93ab61e96b9d052875d8af272e6abb9eeee2590f
Conor Turner <conorturner@live.com>
2017-10-20 22:44:03 +0100

    ha type script is easy

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
 // In the beginning there was a comment
 
+export class BlackHawk {
+    id: number;
+    constructor(id: number) { this.id = id; }
+    logId() {
+        console.log(`My id is ${this.id}!`);
+    }
+}
---
+++ test/test-typestrict.js
@@ -0,0 +1,11 @@
+const {expect} = require("chai");
+
+describe("Test", () => {
+
+	const {BlackHawk} = require("../build");
+	const bh = new BlackHawk(10);
+
+	bh.logId();
+
+
+});
