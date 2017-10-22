fcd4ed1d95960e57054afd05a5ced543094ccbd4
Conor Turner <conorturner@live.com>
2017-10-22 22:13:35 +0100

    abstract the abstractions

--- library/Stream/Stream.ts
+++ library/Stream/Stream.ts
@@ -1,7 +1,12 @@
 "use strict";
 
-export class Stream<T> {
+import {Promise} from 'es6-promise';
+
+export abstract class Stream<T> {
     private buffer: Queue<T>;
+    private _onWrite: Function;
+
+    abstract _transform (item: T): Promise<T>;
 
     constructor(highWaterMark: number) {
         this.buffer = new Queue<T>(highWaterMark);
@@ -16,11 +21,23 @@ export class Stream<T> {
     }
 
     write(item: T): boolean { // used externally to add data to the stream
-        return false;
+        return this.buffer.push(item);
     }
 
     writeMany(items: Array<T>): boolean | Array<T> { // used externally to add data to the stream
-        return false;
+        return items.reduce((acc, item) => {
+            // seems a bit shit, should stop when it hits false
+            if (!this.buffer.push(item)) acc.push(item);
+            return acc;
+
+        }, new Array<T>(0));
+    }
+
+    onWrite(eventHandler: Function): void {
+        this._onWrite = (item: T) => {
+            // Do some internal shit here
+            eventHandler(item);
+        };
     }
 
 
@@ -36,16 +53,16 @@ export class Queue<T> {
     }
 
     push(item: T): boolean {
-        if(this.data.length > this.length - 1) return false;
+        if (this.data.length > this.length - 1) return false;
         else {
             this.data.push(item);
-            return false;
+            return true;
         }
 
     }
 
     pull(): T | null {
-        if(this.data.length === 0) return null;
+        if (this.data.length === 0) return null;
         return this.data.splice(0, 1)[0];
     }
 
